3ef96015cad5638595b6d1e904139e57e40ac4b6
Prem Pathuri <prem1835213@gmail.com>
2022-10-24 15:41:09 +0000

    pose estimation fix

--- src/rb5_ros/pose_estimator/src/pose_estimation_node.py
+++ src/rb5_ros/pose_estimator/src/pose_estimation_node.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import rospy
+import time
 import tf
 import numpy as np
 from tf import LookupException
@@ -26,6 +27,12 @@ TYPE4_ROTATION = np.array([
 	[-1, 0, 0],
 	[0, 0, -1],
 	[0, -1, 0]])
+
+CAM_ROTATION = np.array([
+	[0, 0, 1],
+	[-1, 0, 0],
+	[0, -1, 0]
+])
 
 class WorldTag:
 
@@ -62,44 +69,38 @@ class PoseEstimator:
 			0: WorldTag(tag_type=1, translation=[1.244, 0.037, 0.432]),
 			1: WorldTag(tag_type=1, translation=[1.197, 2.033, 0.399]),
 			2: WorldTag(tag_type=1, translation=[1.286, 0.885, 0.4195]),
-			3: WorldTag(tag_type=2, translation=[0.505, 2.13, 0.374]),
+			3: WorldTag(tag_type=2, translation=[0.711, 2.13, 0.374]),
 			4: WorldTag(tag_type=3, translation=[-0.281, 1.157, 0.285]),
 			5: WorldTag(tag_type=3, translation=[-0.281, 0.386, 0.285]),
 			7: WorldTag(tag_type=4, translation=[0.148, -1.922, 0.339])
 		}
+		# self.robot_T_cam = np.hstack([CAM_ROTATION, np.array([0.05, 0.02, 0.171]).reshape(3, 1)])
+		# self.robot_T_cam = np.vstack([self.robot_T_cam, np.array([0, 0, 0, 1]).reshape(1, 4)])
+
 		self.r = rospy.Rate(10)
-	def tf_callback(self, transformations):
-		for tag_id in self.markers:
-			try:
-				(trans, rot) = self.tl.lookupTransform("camera", "marker_{}".format(tag_id), rospy.Time(0))
-				# pose of tag in camera --> pose of camera in tag
-				cam_P_tag = t.concatenate_matrices(t.translation_matrix(trans), t.quaternion_matrix(rot))
-				tag_P_cam = t.inverse_matrix(cam_P_tag)
-				
-				world_P_cam = np.matmul(self.markers[tag_id].w_T_tag, tag_P_cam) # 4 x 4 matrix
-				cam_position = t.translation_from_matrix(world_P_cam) # length 3 vector
-				cam_quaternion = t.quaternion_from_matrix(world_P_cam) # convert to quaternion
-
-				tf_msg = gm.TransformStamped()
-				tf_msg.header.stamp = rospy.Time.now()
-				tf_msg.header.frame_id = "world"
-				tf_msg.child_frame_id = "camera"	
-
-				tf_msg.transform.rotation = cam_quaternion
-
-				tf_msg.transform.translation.x = cam_position[0]
-				tf_msg.transform.translation.y = cam_position[1]
-				tf_msg.transform.translation.z = cam_position[2]
-
-				self.tb.sendTransform(cam_position, cam_quaternion, rospy.Time.now(), "camera", "world")
-				print("published transform for camera based upon tag {}".format(tag_id))
-			except:
-				pass 
-			
-			self.r.sleep()
+	def april_callback(self, array_msg):
+		detections = array_msg.detections
+		ids_found = [det.id for det in detections]
+		translations = []
+		rotations = []
+		for tag_id in ids_found:
+				try:
+					trans, rot = self.tl.lookupTransform("camera", "marker_{}".format(tag_id), rospy.Time(0))
+					cam_T_tag = t.concatenate_matrices(t.translation_matrix(trans), t.quaternion_matrix(rot))
+					tag_T_cam = t.inverse_matrix(cam_T_tag)
+					world_P_cam = np.matmul(self.markers[tag_id].w_T_tag, tag_T_cam)
+					trans = t.translation_from_matrix(world_P_cam)
+					rot = t.quaternion_from_matrix(world_P_cam)
+					translations.append(trans)
+					rotations.append(rot)
+					if tag_id in [0, 3, 4, 7]:
+						self.tb.sendTransform(trans, rot, rospy.Time.now(), "camera", "world")
+						print("Published world transform based on tag {}".format(tag_id))
+				except LookupException:
+					continue
 
 if __name__ == "__main__":
 	rospy.init_node("pose_estimator")
 	pose_node = PoseEstimator()
-	rospy.Subscriber("/tf", gm.TransformStamped, pose_node.tf_callback, queue_size=10)
+	rospy.Subscriber("/apriltag_detection_array", AprilTagDetectionArray, pose_node.april_callback, queue_size=10)
 	rospy.spin()
